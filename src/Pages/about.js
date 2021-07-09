4afc81cfd67a43fd7d9d7b025b600c878051edf9
beep-boop-stof <loveleenbassi761@gmail.com>
2021-07-08 21:25:06 -0700

    adding a temp Navbar

---
+++ src/Pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'Right',
+		alignItems: 'Right',
+		height: '100vh'
+	}}
+	>
+	<h1>All u need to know rn is that the person creating this page is still working on it. </h1>
+	</div>
+);
+};
+
+export default About;
---
+++ src/Pages/events.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Events = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'Right',
+		alignItems: 'Right',
+		height: '100vh'
+	}}
+	>
+	<h1>Probably not gonna have any events but i guess i made a tab for it anyways!</h1>
+	</div>
+);
+};
+
+export default Events;
